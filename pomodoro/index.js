ef03d9afe82540b99f158b5ad5268716ad95d57b
smophat7 <66281828+smophat7@users.noreply.github.com>
2020-07-06 13:49:03 -0400

    Pomodoro basic functionality complete and added to portfolio
    Needs to get cleaned up for sure. Add more CSS and JS abilities for settings, FAQ page, going straight from timerType to timerType, and keeping track of progress in a session, to name a few.

--- pomodoro/index.js
+++ pomodoro/index.js
@@ -1,35 +1,84 @@
 // NOTE TO SELF:
+  //Reduce the "Timer Type Controls" buttons into one loop (like in the memory game)? Not sure if that would work
   // Make it so that the stop and reset buttons do something, style them later.
-  // Have some larger counts going to display overall session progress (how many pomodoros until the big break, for example) and to be able
-    // to shift between the three different timer types
+  // Have some larger counts going to display overall session progress (how many pomodoros until
+    // the big break, for example) and to be able to shift between the three different timer types
 
 
-var timerMins = 25;
+
+// Default timer settings than can be changed by the user
+
+// Variable initialization
+var timeTypes = {
+  pomodoro: 25,           // Default should be 25 minutes
+  shortBreak: 5,
+  longBreak: 15
+};
+var currentTimerSetting = "pomodoro";     // Default Timer Type setting upon pageload
+var timerMins = timeTypes[currentTimerSetting];
 var timerSecs = timerMins * 60;
+var timerRunning = {};    // will be held by setInterval("Decrement()", 1000) on $(#start-button).click()
 
 
-// Button Controls
+// Timer Type Controls
+$("#pomodoro-button").click(function() {
+  updateTimerType("pomodoro");
+});
+
+$("#short-break-button").click(function() {
+  updateTimerType("shortBreak");
+});
+
+$("#long-break-button").click(function() {
+  updateTimerType("longBreak");
+});
+
+
+// Timer Playing Controls
 $("#start-button").click(function() {
-  setInterval("Decrement()", 1000);
+  timerRunning = setInterval("Decrement()", 1000);
 });
 
 $("#stop-button").click(function() {
-  // stop the timer
+  clearInterval(timerRunning);
 });
 
 $("#reset-button").click(function() {
-  // reset the timer
+  updateTimeValues();
 });
 
 
+// Changing Timer Types and Resetting Clocks
+function updateTimerType(newType) {
+  currentTimerSetting = newType;
+  updateTimeValues();
+  if (newType === "pomodoro") {
+    $(".timer-type").text("Pomodoro");
+  }
+  else if (newType === "shortBreak") {
+    $(".timer-type").text("Short Break");
+  }
+  else if (newType === "longBreak") {
+    $(".timer-type").text("Long Break");
+  }
+}
+
+function updateTimeValues() {
+  timerMins = timeTypes[currentTimerSetting];
+  timerSecs = timerMins * 60;
+  $(".minutes").text(getMinutes());
+  $(".seconds").text(getSeconds());
+}
+
 // Countdown Functionality
 function Decrement() {
-  minutes = $(".minutes");
-  seconds = $(".seconds");
-  minutes.text(getMinutes());
-  seconds.text(getSeconds());
   timerSecs--;
-  //setInterval(Decrement(), 1000);
+  $(".minutes").text(getMinutes());
+  $(".seconds").text(getSeconds());
+  if (timerSecs === 0) {
+    clearInterval(timerRunning);
+    $(".timer-type").text("Time's Up!")
+  }
 }
 
 function getMinutes() {
@@ -38,5 +87,13 @@ function getMinutes() {
 }
 
 function getSeconds() {
-  return timerSecs - Math.round(timerMins * 60);
+  // Function returns info based on the value of timerSecs but does not change it
+  // It is changed in Decrement()
+  var displaySeconds = timerSecs - Math.round(timerMins * 60);
+  if (displaySeconds < 10) {
+    return "0" + displaySeconds;
+  }
+  else {
+    return displaySeconds;
+  }
 }
